1a4c9554a35dad7fefbb26c6dd78fd1477481c80
NicholasCo <n.cotterill11@googlemail.com>
2016-11-04 15:03:14 +0000

    finished Rectangle class

--- com.fourSided/src/com/fourSided/Shape/Rectangle.java
+++ com.fourSided/src/com/fourSided/Shape/Rectangle.java
@@ -1,5 +1,43 @@
 package com.fourSided.Shape;
 
-public class Rectangle {
+public class Rectangle extends Parallelogram{
 
+	public Rectangle(Point point1, Point point2, Point point3,Point point4) throws ShapeException {
+//			if(isRectangle( point1, point2, point3, point4)){
+//				return;
+//			}
+//			else{
+//				throw new ShapeException(null, point1, point2, point3, point4);
+//			}
+		
+		super(point1, point2, point3, point4);
+		Rectangle.isRectangle(this);
+	}
+	
+	static public boolean isRectangle(Quadrilateral q) throws ShapeException{
+		Line line1=q.getSide1();
+		Line line2=q.getSide2();
+		Line line3=q.getSide3();
+		Line line4=q.getSide4();
+		if(line1.getSlope(line1.getP1(), line1.getP2())==line3.getSlope(line3.getP1(), line3.getP2())){
+			if ((line1.getLength()==line3.getLength())&(line2.getLength()==line4.getLength())){
+					return true;
+			}
+		}
+		else{
+			throw new ShapeException(q,q.getPoint1(),q.getPoint2(),q.getPoint3(),q.getPoint4());
+		}
+		return false;
+	}
+	
+	
+	public double GetArea(Quadrilateral q) {
+		Line line1=q.getSide1();
+		Line line2=q.getSide2();
+		double length=line1.getLength();
+		double width=line2.getLength();
+		double area=length*width;
+		return area;
+	}
+	
 }
--- com.fourSided/src/com/fourSided/Test/RectangleTest.java
+++ com.fourSided/src/com/fourSided/Test/RectangleTest.java
@@ -1,13 +1,51 @@
+/**
+ * 
+ */
 package com.fourSided.Test;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.fourSided.Shape.Point;
+import com.fourSided.Shape.Quadrilateral;
+import com.fourSided.Shape.Rectangle;
+import com.fourSided.Shape.ShapeException;
+
+/**
+ * @author Administrator
+ *
+ */
 public class RectangleTest {
 
+
 	@Test
-	public void test() {
+	public void testRectangle(){
+		Point point1= new Point(0,3);
+		Point point2= new Point(2,3);
+		Point point3= new Point(2,0);
+		Point point4= new Point(0,0);
+		try{
+			
+		}
+		Rectangle rectangle=new Rectangle(point1,point2,point3,point4);
+		assertEquals("Not a Rectangle",rectangle);
+	}
+	
+	@Test
+	public void testRectangle() {
+		Point point1= new Point(0,3);
+		Point point2= new Point(2,3);
+		Point point3= new Point(2,0);
+		Point point4= new Point(0,0);
+		Rectangle rectangle=new Rectangle(point1,point2,point3,point4);
+		assertEquals("Not a Rectangle",rectangle);
+	}
+
+
+	@Test
+	public void testGetArea() {
+		
 		fail("Not yet implemented");
 	}
 
